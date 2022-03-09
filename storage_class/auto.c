2272ed826104fa57538c7f815434e80513384486
root <root@DESKTOP-S9BD952.localdomain>
2022-03-09 20:22:10 +0530

    Added storage class

---
+++ storage_class/auto.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{ 		//function scope start
+	int a=10;  
+	{	//block scope start
+		int a=20;
+		printf("value of a in block scope is %d\n",a);
+	}	//block scope end
+	printf("value of a in function scope is %d\n",a);
+} 		//function scope end
