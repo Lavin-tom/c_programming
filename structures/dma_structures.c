2693542ea38a804bbf70b2542b04e03bdac26556
Lavin-tom <lavintom007@gmail.com>
2022-03-29 21:00:47 +0530

    modify some code

--- structures/dma_structures.c
+++ structures/dma_structures.c
@@ -10,13 +10,13 @@
 	};
 int main()
 {
-	printf("enter students details\n");
-	struct student *s1[2];
-
-	//s1[i]=struct* malloc(sizeof(student));
-	for(int i=0;i<2;i++)
+	int n;
+	printf("enter no of students to be added\n");
+	scanf("%d",&n);
+	typedef struct student student;
+	 student *s1=malloc(n*sizeof(student));
+	for(int i=0;i<n;i++)
 	{
-		s1[i]=struct* malloc(sizeof(student));
 		printf("enter the rollno\n");
 		scanf("%d",&s1[i].rollno);
 		printf("enter the name\n");
@@ -24,9 +24,9 @@ int main()
 		printf("enter the mark\n");
 		scanf("%f",&s1[i].mark);
 	}
-	for(int i=0;i<2;i++)
+	for(int i=0;i<n;i++)
 	{
-		printf("student %d\n",i+1);
-		printf("roll no :%d name: %s marks: %f\n",s1[i].rollno,s1[i].name,s1[i].mark);
+		printf("\nstudent %d\n",i+1);
+		printf("roll no :%d name: %s marks: %.f\n",s1[i].rollno,s1[i].name,s1[i].mark);
 	}
 }
