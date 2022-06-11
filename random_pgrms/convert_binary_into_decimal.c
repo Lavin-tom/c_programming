f529e9789d71c74c4b02e241727440186d6c5ff7
Lavin-tom <lavintom007@gmail.com>
2022-06-11 20:34:40 +0530

    modify some codes

--- random_pgrms/convert_binary_into_decimal.c
+++ random_pgrms/convert_binary_into_decimal.c
@@ -1,6 +1,7 @@
 //convert binary to decimal no
 
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
 	int n,k=1,sum=0;
@@ -10,10 +11,20 @@ int main()
 	while(n>0)
 	{
 		int l=n%10;
-		int m=l*k;
-		sum=sum+m;
-		n=n/10;
-		k=k*2;
+		if(l==0 || l==1)
+		{
+			int m=l*k;
+			sum=sum+m;
+			n=n/10;
+			k=k*2;
+		}
+		else
+		{
+			printf("enter valid binary no\n");
+			exit(0);
+		}
 	}
 	printf("equalent decimal: %d\n",sum);
+	printf("equalent hexadecimal: %x\n",sum);
+	printf("equalent octal: %o\n",sum);
 }
