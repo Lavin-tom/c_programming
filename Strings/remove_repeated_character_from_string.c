994cd5ee3e8184709383221b12e2c82f985eb2f1
Lavin-tom <lavintom007@gmail.com>
2022-07-26 21:51:30 +0530

    some files modified

--- Strings/remove_repeated_character_from_string.c
+++ Strings/remove_repeated_character_from_string.c
@@ -1,30 +1,31 @@
 //wap to remove repeated character from a string
-//not completed
 #include<stdio.h>
 #include<string.h>
 int main()
 {
 	char string[10];
-	int str_length;
+	int str_length,count=0;
 	printf("enter string\n");
 	scanf("%s",string);
 	str_length=strlen(string);
 
 	for(int i=0;i<str_length;i++)
 	{
-		int count=0;
 		for( int j=0;j<str_length;j++)
 			{
 				if(string[i]==string[j])
 				{
 					count++;
-					if(count==1)
-					//string[i]=string[i+1];
-				//	j--;
-					printf("%c ",string[i]);
+					if(count>1)
+					{
+						//left shift if duplicate found
+						for(int k=j;k<str_length;k++)
+						string[k]=string[k+1];
+					}
 				}
 			}
+		count=0;
 	}
-	//printf("%s\n",string);
+	printf("%s\n",string);
 }
 
