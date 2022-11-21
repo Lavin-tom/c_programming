b28970e5b0c2cc68a56bd991c7a7a265d5d1e351
Lavin-tom <lavintom007@gmail.com>
2022-11-21 21:55:36 +0530

    added more codes

--- array/array_duplicate_element.c
+++ array/array_duplicate_element.c
@@ -54,7 +54,7 @@ int main()
         }
 
 	printf("\n");
-        for(int i=0;i<2;i++){
+        for(int i=0;i<1;i++){
                 for(int j=0;j<7;j++)
                         printf("%d ",sort[i][j]);
         printf("\n");
