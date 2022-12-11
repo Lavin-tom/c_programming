558117bd95e9baf2baad084aca229838ad822990
Lavin-tom <lavintom007@gmail.com>
2022-12-11 20:27:34 +0530

    added more codes

--- cmnd_line_argument/sum_of_nos_using_cla.c
+++ cmnd_line_argument/sum_of_nos_using_cla.c
@@ -1,11 +1,17 @@
 //sum of integers using command line arguments
 //we need atoi function to convert string into integer
 #include<stdio.h>
+#include<stdlib.h>
 int main(int argc,char *argv[])
 {
 	int sum=0;
-	for(int i=0;argv[1][i];i++)
-		sum=sum+(argv[1][i]);
+	if(argc==1){
+		printf("required atleast one input\n");
+		return 0;
+	}
+	for(int i=1;argv[i];i++)
+		sum=sum+atoi(argv[i]);
+
 	printf("argc: %d\n",argc);
 	printf("sum : %d\n",sum);
 }
