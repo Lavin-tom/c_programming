ed48e7423ccf23ce42d7a35db7c82902e4a3c283
Lavin-tom <lavintom007@gmail.com>
2022-12-14 22:06:33 +0530

    added more codes

--- coding_challenge/clock_hands.c
+++ coding_challenge/clock_hands.c
@@ -1,18 +1,22 @@
 //create a function 
 //input is time in 12 hr format
 //function return smallest angle b/w the minute hand and hour hand
-//not yet completed
+//
 #include<stdio.h>
 int main()
 {
 	int hh,mm,ss;
-	float d;
+	float h,m,d;
 	printf("enter time in 12 hr format\n");
 	scanf("%d %d %d",&hh,&mm,&ss);
 	
-	d=(30*hh)-((11/2) * mm);
-	printf("%f\n",d);
-	if(d>180)
-		d=360-d;
-	printf("%f\n",d);
+	//equation for angle b/w hour hand and minute hand from wikipedia
+	h = 0.5 *((60 * hh) - (11 * mm));
+
+	printf("%f\n",h);
+	
+	//if angle is >180 then subtract from 360
+	if(h>180)
+		h=360-h;
+	printf("%f\n",h);
 }
