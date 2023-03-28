19e3177e81d4de4e0aac82e8e955f2bc3d07f137
LAVIN TOM K ABRAHAM <53207510+Lavin-tom@users.noreply.github.com>
2023-03-28 15:36:54 +0530

    Added 2d array code with Malloc

---
+++ dynamic_memory_allocation/malloc/two_d_array_using_malloc.c
@@ -0,0 +1,43 @@
+//2d array using dma
+//dynamic memory allocation 
+
+#include<stdio.h>
+#include<stdlib.h>
+#define raw 3
+#define column 3
+void setArray(int **arr)
+{
+    for(int i=0;i<raw;i++)
+    {
+        for(int j=0;j<column;j++)
+        {
+            arr[i][j] = i+j;
+        }
+    }
+}
+void getArray(int **arr)
+{
+    for(int i=0;i<raw;i++)
+    {
+        for(int j=0;j<column;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+void deallocate(int *arr[])
+{
+        for(int i=0;i<raw;i++)
+        free(arr[i]);
+}
+int main()
+{
+    int *arr[raw];
+    for(int i=0;i<raw;i++)
+        arr[i] = (int*)malloc(column*sizeof(int));
+
+    setArray(arr);
+    getArray(arr);
+    deallocate(arr);
+}
