a318850e3cba7d25bc43afb73dbebb8fe00b1cdf
Lavin-tom <lavintom007@gmail.com>
2023-04-13 17:26:23 +0530

    added more codes

---
+++ functions/call_by_reference.c
@@ -0,0 +1,19 @@
+// call by reference
+
+#include <stdio.h>
+void swap(int *, int *);
+int main()
+{
+    int a, b;
+    printf("enter a and b\n");
+    scanf("%d %d", &a, &b);
+    swap(&a, &b);
+    printf("In main function: a: %d b: %d\n", a, b);
+}
+void swap(int *a, int *b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+    printf("In swap function: a: %d b: %d\n", *a, *b);
+}
